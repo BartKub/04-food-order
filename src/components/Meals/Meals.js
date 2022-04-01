f8768aafa7a0a78b6007bec2e14d9bb2a6aa08ae
Bartłomiej Kubica <bartlomiej.kubica@ext.nordiska.com>
2022-04-01 12:20:26 +0200

    meals

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import MealsSummary from "./MealsSumary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
